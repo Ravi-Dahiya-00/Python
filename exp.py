7656331a85c9e43903028dc3eef86099a03362f8
Ravi Dahiya <ravidahiya9350@gmail.com>
2024-08-04 12:05:36 +0530

    New commit

---
+++ exp.py
@@ -0,0 +1,18 @@
+import turtle
+turtle.color("black")
+turtle.penup()
+turtle.goto(-100,0)
+turtle.pendown()
+turtle.circle(90)
+
+turtle.color("black")
+turtle.penup()
+turtle.goto(100,0)
+turtle.pendown()
+turtle.circle(90)
+
+turtle.color("pink")
+turtle.penup()
+turtle.goto(0,60)
+turtle.pendown()
+turtle.circle(12)
