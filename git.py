37b001e737205e7c33562264379cdaa3d61b168c
Ravi Dahiya <ravidahiya9350@gmail.com>
2024-08-08 22:26:40 +0530

    learning

--- git.py
+++ git.py
@@ -1,7 +1,1 @@
-print(2 + 8)  #addition of two numbers. +
-print(9 - 5)  #substraction of two numbers. -
-print(5 * 3)  #multiplication of two numbers. *
-print(5 / 3)  #division of two numbers. /
-print(
-    7 // 2
-)
+prinnt("hello")
